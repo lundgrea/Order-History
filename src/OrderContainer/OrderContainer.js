2a2cf1b8ed908832a1d6dc1ac98574c9c3f3d1de
Alyssa Lundgren <lundgrea@gmail.com>
2019-08-29 09:46:53 -0600

    Add Order Component and save orders to state

--- src/OrderContainer/OrderContainer.js
+++ src/OrderContainer/OrderContainer.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import Order from '../Order/Order'
 
 const OrderContainer = ({orders}) => {
+  console.log('within ordercontainer', orders)
   const listedOrders = orders.map(order => {
     return (
       <Order
-        item={order.item}
+        name={order.name}
+        description={order.description}
+        price={order.img}
+        img={order.img}
+        id={order.id}
+        key={order.id}
       />
     )
   })
